61f9dad43fbfad7b870d5ca9f4c76a355304aa59
Jarryd Pearson <github@pearsonfamily.id.au>
2016-12-13 19:13:11 +1100

    / ‘update.php’

--- update.php
+++ update.php
@@ -2,8 +2,24 @@
 
 ?>
 <div id="effect">
-
+	<table>
+		<tr>
+			<td>
+				Effect
+			</td>
+			<td>
+				Description
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php echo $effect; ?>
+			</td>
+		</tr>
+	</table>
 </div>
 <div id="bee">
+	<table>
 
+	</table>
 </div>
